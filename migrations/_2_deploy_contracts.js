8fccbcb31024a13887f373a6d0468e82ae10b1b4
quark8 <8quark@gmail.com>
2017-11-29 23:21:29 +0300

    migrations

--- migrations/_2_deploy_contracts.js
+++ migrations/_2_deploy_contracts.js
@@ -1,8 +1,28 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
-//
-// module.exports = function(deployer) {
-//   deployer.deploy(ConvertLib);
-//   deployer.link(ConvertLib, MetaCoin);
-//   deployer.deploy(MetaCoin);
-// };
+'use strict';
+
+const _owners = [
+    '',
+    '',
+    ''
+];
+
+const Token = artifacts.require("MetropolToken.sol");
+const FundsRegistry = artifacts.require("FundsRegistry.sol");
+const Crowdsale = artifacts.require("MetropolCrowdsale.sol");
+
+
+module.exports = function(deployer, network) {
+    let _token, _funds, _sale;
+
+    deployer.then(function() {
+        return Token.new(_owners);
+    }).then(function(instance) {
+        _token = instance;
+
+        return FundsRegistry.new(_owners, 2, 0);
+    }).then(function(instance) {
+        _funds = instance;
+
+        return Crowdsale(_token.address, _funds.address, _owners);
+    });
+};
